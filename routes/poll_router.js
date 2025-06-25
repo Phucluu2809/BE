47aa88ddd5d162b7277ac18a568f39e4d348c28f
Phucluu2809 <thanhtam.141019830@gmail.com>
2025-06-25 16:32:02 +0700

    aaa

--- routes/poll_router.js
+++ routes/poll_router.js
@@ -12,7 +12,7 @@ const router = Router();
     router.get('/', async (req, res, next) => {
         try {
             const users = await userController.getAllUser();
-            res.json({ message: "hello" });
+            res.send("xin chao moi nguoi" );
             
         } catch (error) {
             console.error('Error in /getAll_information:', error);
